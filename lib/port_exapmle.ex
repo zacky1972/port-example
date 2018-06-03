556fad49a254368552217c0ba67468f49318dd85
Susumu Yamazaki <zacky1972@gmail.com>
2018-06-03 14:02:55 +0900

    initial commit

---
+++ lib/port_exapmle.ex
@@ -0,0 +1,18 @@
+defmodule PortExapmle do
+  @moduledoc """
+  Documentation for PortExapmle.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PortExapmle.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/port_exapmle_test.exs
@@ -0,0 +1,8 @@
+defmodule PortExapmleTest do
+  use ExUnit.Case
+  doctest PortExapmle
+
+  test "greets the world" do
+    assert PortExapmle.hello() == :world
+  end
+end
